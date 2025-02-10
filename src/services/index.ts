ebf4d069ebbc214f06c1cdcf4e24d8ea7be2d839
idan.o <Idan.o@solaredge.com>
2025-02-10 13:26:37 +0200

    .

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,7 @@
 import { config, getConfig, updateConfig, type Config } from './config';
 import patientInfo from './patientInfo';
 import patientTimeTracking from './patientTimeTracking'; 
-import meetingService, {type Meeting} from "./meeting";
+import meetingService, {type Meeting} from "./Meeting";
 
 export {
     patientInfo,
